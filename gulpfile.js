cb7790642b7cdc0224e4a1ca1d2b2c7b86d1b6aa
alex love <alove2096@gmail.com>
2019-02-01 13:05:22 -0800

    updated gulp file, need to work on the proper way to build it

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,15 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var pkg = require('./package.json');
-var CleanCSS = require('clean-css');
-var input = 'a{font-weight:bold;}';
-var options = { /* options */ };
-var output = new CleanCSS(options).minify(input);
+var CleanCSS = require('gulp-clean-css');
 
 gulp.task('css', function(){
    return gulp.src('css/full-width-pics.css')
   .pipe(CleanCSS())
   .pipe(gulp.dest('dist'))
-})
-
-// Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
-
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
-
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
-
-})
-
-// Default task
-gulp.task('default', ['vendor']);
-
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
 });
 
-// Dev task
-gulp.task('dev', ['browserSync'], function() {
-  gulp.watch('./css/*.css', browserSync.reload);
-  gulp.watch('./*.html', browserSync.reload);
-});
+gulp.task('html', function(){
+  return gulp.src('index.html')
+  .pipe(gulp.dest('build'))
+})
+
+
