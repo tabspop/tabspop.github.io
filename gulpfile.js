f2f1ed498f694137d54b0c4cc0e1a63419205e55
alex love <alove2096@gmail.com>
2019-02-12 15:01:44 -0800

    updating

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var CleanCSS = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
 
 gulp.task('css', function(){
    return gulp.src('css/full-width-pics.css')
@@ -10,6 +11,21 @@ gulp.task('css', function(){
 gulp.task('html', function(){
   return gulp.src('index.html')
   .pipe(gulp.dest('build'))
+  .pipe(browserSync.stream({match: 'index.html'}));
 })
 
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      injectChanges: true,
+      server: {
+          baseDir: "/build"
+      }
+  });
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('index.html', gulp.series('html','browser-sync'));
+  gulp.watch('css/full-width-pics.css',gulp.series('css'));
+});
 
